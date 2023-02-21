93660f06c8accc2cf3346d6363d428cceeca2547
juninkorea95 <juninkorea95@gmail.com>
2023-02-21 23:35:02 +0900

    코드 수정

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('./db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const path = require('path');
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(4000, () => {
-    console.log('JSON Server is running')
-})
+const fs = require('fs'); //서버 배포 단계에서 /tmp 에러 발생하기 때문에 추가
+const os = require('os'); //서버 배포 단계에서 /tmp 에러 발생하기 때문에 추가
 
-// Export the Server API
-module.exports = server
+
+//배포 deploy 과정에서 tmp경로안에 db.json을 찾을수 없는 에러 발생하여 로직 추가
+fs.copyFile('db.json', os.tmpdir() + '/db.json', function (err) {
+  if (err) console.log(err);
+  else console.log('copy file succeed to' + os.tmpdir());
+});
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(os.tmpdir() + '/db.json'));
+const middlewares = jsonServer.defaults({
+  static: path.resolve(__dirname + '/../build/'),
+});
+
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+server.use(router);
+server.listen(port, () => {
+  console.log('JSON Server is running');
+});
