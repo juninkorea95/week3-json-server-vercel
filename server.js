9326ca76995a0a0d0de4ede87f697b00174d8a78
juninkorea95 <juninkorea95@gmail.com>
2023-02-21 23:15:51 +0900

    다시 배포

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router("./db.json");
+const router = jsonServer.router('./db.json')
 const middlewares = jsonServer.defaults()
-
-server.use(router);
 
 server.use(middlewares)
 // Add this before server.use(router)
@@ -12,7 +10,7 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
-
+server.use(router)
 server.listen(4000, () => {
     console.log('JSON Server is running')
 })
